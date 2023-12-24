35a326ce6797634d426510b2f933a4d1ec31f0bd
Saurav S <77258135+sauravss98@users.noreply.github.com>
2023-12-24 15:42:38 +0000

    first route done

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const apiRoutes = require("./routes/apiRoutes");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Api Running" });
 });
+
+app.use("/api", apiRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
